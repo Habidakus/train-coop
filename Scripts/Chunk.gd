5ff6de4417209d71205a226d045a2a229fb5a519
Jason Abbott <woebane@gmail.com>
2024-07-16 12:26:23 -0700

    Added fog and ground material, added basic box cars, more valley maneuvering room

--- Scripts/Chunk.gd
+++ Scripts/Chunk.gd
@@ -7,34 +7,69 @@ var noise : Noise
 var x : float
 var z : float
 var height : float
+var material : Material
 
-func c_init(inoise : Noise, ix : float, iz : float, ichunk_size : float, isubdivide : int, iheight : float):
+func c_init(inoise : Noise, key : Vector2i, ichunk_size : float, isubdivide : int, iheight : float, imaterial : Material):
 	noise = inoise
-	x = ix
-	z = iz
+	x = key.x * ichunk_size
+	z = key.y * ichunk_size
 	chunk_size = ichunk_size
 	subdivide = isubdivide
 	height = iheight
+	material = imaterial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	generate_chunk()
 
 func get_height(ix : float, iz : float):
-	const rrwidth :float = 32
-	const rry : float = 0.0
-	var offset : float = abs(ix) - rrwidth
-	if offset < 0:
-		return rry
-	var y = noise.get_noise_2d(ix, iz)
-	if offset < rrwidth:
-		offset /= rrwidth
-		offset = 1 - offset
-		offset = offset * offset
-		return rry * offset + y * (1 - offset)
-	if offset > 256:
-		y = y * pow(2, ((offset - 256.0) / 256.0))
-	return y
+	var x_offset = abs(ix)
+	
+	const rr_width : float = 32
+	const rr_height : float = 0.0
+	
+	var rr_weight : float = 1.0 - ((x_offset / rr_width) * (x_offset / rr_width))
+	if rr_weight < 0:
+		rr_weight = 0
+
+	const valley_width : float = 512
+	var valley_weight : float = 1
+	if x_offset < valley_width:
+		valley_weight = x_offset / valley_width
+		valley_weight = sqrt(valley_weight)
+	var valley_height : float = 0
+	if valley_weight > 0:
+		valley_height = noise.get_noise_2d(ix, iz) / 2.0
+	else:
+		valley_weight = 0
+	
+	var hill_width : float = 2048
+	var hill_offset = x_offset - valley_width
+	if hill_offset < 0:
+		hill_offset = 0
+	var hill_weight : float = (hill_offset / hill_width)
+	var hill_height : float = 0
+	if hill_weight > 0:
+		const hill_scale : float = 32.0
+		hill_height = (noise.get_noise_2d(ix / hill_scale, iz / hill_scale) + 0.25) * 64.0
+		if hill_weight > 1:
+			hill_weight = 1
+	else:
+		hill_weight = 0
+	
+	return rr_weight * rr_height + valley_weight * valley_height + hill_weight * hill_height / (rr_weight + valley_weight + hill_weight)
+	#var offset : float = abs(ix) - rrwidth
+	#if offset < 0:
+	#	return rry
+	#var y = noise.get_noise_2d(ix, iz)
+	#if offset < rrwidth:
+	#	offset /= rrwidth
+	#	offset = 1 - offset
+	#	offset = offset * offset
+	#	return rry * offset + y * (1 - offset)
+	#if offset > 256:
+	#	y = y * pow(2, ((offset - 256.0) / 256.0))
+	#return y
 
 func generate_chunk():
 	var plane_mesh = PlaneMesh.new()
@@ -61,14 +96,17 @@ func generate_chunk():
 		var dy = get_height(dx, dz)
 		vertex.y = dy * height
 		data_tool.set_vertex(i, vertex)
-		var dxn = dx - 0.1
-		var dzn = dz - 0.1
-		var dyxnz = get_height(dxn, dz)
-		var dyxzn = get_height(dx, dzn)
-		var a = Vector3(dxn, dyxnz, dz) - Vector3(dx, dz, dy)
-		var b = Vector3(dx, dyxzn, dzn) - Vector3(dx, dz, dy)
-		var crossProduct = a.cross(b)
-		data_tool.set_vertex_normal(i, crossProduct.normalized())
+		
+		#data_tool.set_vertex_color(i, Color.RED)
+		
+		#var dxn = dx - 0.1
+		#var dzn = dz - 0.1
+		#var dyxnz = get_height(dxn, dz)
+		#var dyxzn = get_height(dx, dzn)
+		#var a = Vector3(dxn, dyxnz, dz) - Vector3(dx, dz, dy)
+		#var b = Vector3(dx, dyxzn, dzn) - Vector3(dx, dz, dy)
+		#var crossProduct = a.cross(b)
+		#data_tool.set_vertex_normal(i, crossProduct.normalized())
 	
 	array_plane.clear_surfaces()
 	#for s in range(array_plane.get_surface_count()):
@@ -81,7 +119,7 @@ func generate_chunk():
 	
 	mesh_instance = MeshInstance3D.new()
 	mesh_instance.mesh = surface_tool.commit();
-	mesh_instance.set_surface_override_material(0, Material.new())
+	mesh_instance.set_surface_override_material(0, material)
 	mesh_instance.create_trimesh_collision()
 	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_ON
 	add_child(mesh_instance)
--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,21 +1,46 @@
 extends Node3D
 
 @export var noise : Noise
-@export var chunk_scene: PackedScene
+@export var chunk_scene : PackedScene
+@export var train_car_scene : PackedScene
+@export var chunk_material : Material
+@export var train_speed_miles_per_hour : float = 50
+@export var box_car_spacing_feet : float = 60
+@export var box_car_count : int = 4
+
+const units_per_meter : float = 10
+const meters_per_mile : float = 1609.34
+const meters_per_foot : float = 0.3048
 
 const chunk_size : int = 1024
-const chunk_amount : int = 4
+const chunk_amount : int = 2
 const chunk_radius : int = int(chunk_amount * 0.5);
 const chunk_subdivide : int = 64
 const chunk_height : float = 64
 
-var created_chunks = {}
+var train_cars = []
+var existing_chunks = {}
 #var building_chunks = {}
 #var thread : Thread
 
 func _ready():
 	randomize()
 	noise.seed = randi()
+	$Camera3D.position.y = 25.0
+	
+	for i in range(0, box_car_count):
+		var train_car = train_car_scene.instantiate()
+		train_car.find_child("CSGBox3D").scale *= units_per_meter
+		train_cars.append(train_car)
+		add_child(train_car)
+		var box_car_spacing_meters : float = box_car_spacing_feet * meters_per_foot
+		train_car.position = $Camera3D.position - Vector3(0, 25, i * box_car_spacing_meters * units_per_meter)
+		print(train_car.position)
+		
+	for z in range(-1, 1):
+		add_chunk(-1, z)
+		add_chunk(0, z)
+		add_chunk(1, z)
 	
 	#thread = Thread.new()
 
@@ -23,45 +48,54 @@ func _process(_delta: float):
 	update_chunks()
 	clean_up_chunks()
 	reset_chunks()
-	$Camera3D.position.y = 25.0
-	$Camera3D.position.z -= 50.0 * _delta
+	
+	var meters_per_second = meters_per_mile * train_speed_miles_per_hour / 3600.0
+	$Camera3D.position.z -= meters_per_second * _delta * units_per_meter
 	#$Camera3D.rotation_degrees.y += _delta
+	var box_car_spacing_meters : float = box_car_spacing_feet * meters_per_foot
+	for i in range(0, box_car_count):
+		train_cars[i].position = $Camera3D.position - Vector3(0, 25, i * box_car_spacing_meters * units_per_meter)
 	
 func update_chunks():
 	var pt = $Camera3D.position
 	var px :int = int(pt.x / chunk_size)
 	var pz :int = int(pt.z / chunk_size)
 	for x in range(px - chunk_radius, px + chunk_radius):
-		for z in range(pz - chunk_amount, pz):
+		for z in range(pz - 4 * chunk_amount, pz):
 			add_chunk(x, z)
 
 func clean_up_chunks():
-	pass
+	var pt = $Camera3D.position
+	var pz :int = int(pt.z / chunk_size)
+	var fadeZ = pz + chunk_amount
+	var all_keys = existing_chunks.keys()
+	for key in all_keys:
+		if key.y >= fadeZ:
+			existing_chunks.erase(key)
 
 func reset_chunks():
 	pass
 	
 func add_chunk(x : int, z : int):
-	var key = str(x) + "," + str(z)
-	if created_chunks.has(key): # || building_chunks.has(key):
+	var key = Vector2i(x, z)
+	if existing_chunks.has(key): # || building_chunks.has(key):
 		return
 	
-	load_chunk(key, x, z)
+	load_chunk(key)
 	#if not thread.is_alive():
 	#	var callable = Callable(self, "load_chunk").bind(thread, x, z)
 	#	thread.start(callable)
 	#	building_chunks[key] = 1
 
-func load_chunk(key: String, x : int, z : int):
+func load_chunk(key: Vector2i):
 	var chunk : Node3D = chunk_scene.instantiate()
-	chunk.c_init(noise, x * chunk_size, z * chunk_size, chunk_size, chunk_subdivide, chunk_height)
-	chunk.position = Vector3(x * chunk_size, 0, z * chunk_size)
+	chunk.c_init(noise, key, chunk_size, chunk_subdivide, chunk_height, chunk_material)
+	chunk.position = Vector3(key.x * chunk_size, 0, key.y * chunk_size)
 	add_child(chunk)
-	created_chunks[key] = chunk
+	existing_chunks[key] = chunk
 
-func get_chunk(x : int, z : int):
-	var key = str(x) + "," + str(z)
-	if created_chunks.has(key):
-		return created_chunks.get(key)
+func get_chunk(key : Vector2i):
+	if existing_chunks.has(key):
+		return existing_chunks.get(key)
 	
 	return null
