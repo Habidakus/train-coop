6e1cffd4a14834d15f16876cd95c4c09f77867ea
Jason Abbott <woebane@gmail.com>
2024-07-22 16:41:58 -0700

    Have the tanks aim and move

--- Scripts/tank_two.gd
+++ Scripts/tank_two.gd
@@ -10,6 +10,7 @@ var state_firing : bool = false
 var state_dead : bool = false
 const max_aim_time : float = 1
 var aim_time : float = 0
+var speed : float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -48,6 +49,8 @@ func get_ground_pos(pos : Vector3):
 	var end_point = pos + Vector3.DOWN * 64
 
 	var query = PhysicsRayQueryParameters3D.create(start_point, end_point)
+
+	# TODO: We should make sure we're only testing against ground, not just anything that isn't ourselves
 	query.exclude = [$StaticBody3D.get_rid()]
 
 	var result : Dictionary = get_world_3d().direct_space_state.intersect_ray(query)
@@ -60,6 +63,8 @@ func get_ground_pos(pos : Vector3):
 func _process(_delta):
 	#if name == "tank2":
 		#print(position)
+	if speed > 0:
+		global_position = get_ground_pos(-1 * speed * _delta * get_global_transform().basis.z.normalized() + global_position) + 2.5 * Vector3.UP
 		
 	var our_ground : Vector3 = get_ground_pos(global_position)
 	if our_ground == Vector3.ZERO:
@@ -68,14 +73,22 @@ func _process(_delta):
 	if forward_ground == Vector3.ZERO:
 		return
 	var forward_pos = global_position + (forward_ground - our_ground)
+	if forward_pos == global_position:
+		print("forward == current!!! rot=", global_rotation_degrees)
 	look_at(forward_pos)
 	global_position.y = (our_ground.y + forward_pos.y) / 2.0 + 2.5
 	
 	if state_dead:
+		if name == "tank2":
+			print("dead: z=", position.z)
 		return
 	
 	if state_firing:
 		#TODO: Make fire
+		speed = 0
+		if name == "tank2":
+			print("firing: z=", position.z)
+			
 		if position.z > world.get_train_end_z():
 			state_firing = false
 			state_advancing = true
@@ -90,29 +103,50 @@ func _process(_delta):
 			#pre_aim_rotation = rotation
 			return
 
-		#TODO: Aim at where we're going
-		#TODO: Avoid other tanks
-		#TODO: Advance in our current aim direction, not just sideways towards our goal
+		#TODO: Avoid other tanks and the train
 		var goal_pos : Vector3 = get_ground_pos(Vector3(global_position.x, global_position.y + 2.5, goal_z)) + Vector3.UP * 2.5
-		global_position += (goal_pos - global_position).normalized() * world.get_speed() * 1.5 * _delta
+		var dot = (goal_pos - global_transform.origin).normalized().dot(get_global_transform().basis.z.normalized() * -1)
+		
+		turn_to(goal_pos, _delta)
+		if dot < 0:
+			speed = 0
+		else:
+			speed = lerp(speed, world.get_speed() * 1.5, _delta)
+		#global_position += (goal_pos - global_position).normalized() * world.get_speed() * 1.5 * _delta
+		if name == "tank2":
+			print("advancing: z=", position.z, " dot=", dot, " speed=", speed)
 		return
 	
 	if state_aiming:
 		aim_time -= _delta
+		speed = lerp(speed, 0.0, _delta * 2)
 		
 		var global_pos = global_transform.origin
 		var target_pos = Vector3(0, global_pos.y, world.get_train_start_z())
-		var rotation_speed = 0.01
-		var wtransform = global_transform.looking_at(target_pos)
-		var wrotation = global_transform.basis.get_rotation_quaternion().slerp(wtransform.basis.get_rotation_quaternion(), rotation_speed)
-		var s = scale
-		global_transform = Transform3D(Basis(wrotation), global_transform.origin)
-		scale = s
+		turn_to(target_pos, _delta)
 		
 		if name == "tank2":
-			print("turning: ", rotation_degrees)
+			#var dot = (target_pos - global_pos).normalized().dot(get_global_transform().basis.z.normalized() * -1)
+			var cross_front = (target_pos - global_pos).normalized().cross(get_global_transform().basis.z.normalized() * -1)
+			var end_pos = Vector3(0, global_pos.y, world.get_train_end_z())
+			var cross_end = (end_pos - global_pos).normalized().cross(get_global_transform().basis.z.normalized() * -1)
+			
+			print("turning: rot=", rotation_degrees.y, " front=", cross_front, " end=", cross_end)
 		
 		#var n : Transform3D = global_transform.looking_at()
 		if aim_time <= 0:
 			state_aiming = false
 			state_firing = true
+
+func turn_to(target_pos : Vector3, delta : float):
+	#var global_pos = global_transform.origin
+	if Vector3.UP.cross(- (target_pos - global_position).normalized()).is_zero_approx():
+		#TODO: This tank is in a bad way, we should fix it getting in this state
+		return
+
+	var wtransform = global_transform.looking_at(target_pos)
+	#TODO: We shouldn't be using lerp here, we should be turning at a constant speed
+	var wrotation = global_transform.basis.get_rotation_quaternion().slerp(wtransform.basis.get_rotation_quaternion(), delta)
+	var s = scale
+	global_transform = Transform3D(Basis(wrotation), global_transform.origin)
+	scale = s
